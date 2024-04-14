42a6dc92d83b9e8b3bf5c11a42f12d53631881d7
syf20020816 <syf20020816@outlook.com>
2024-04-15 00:43:22 +0800

    构建AppMain实例 23%

--- gen/generator/makepad/src/widget/model/mod.rs
+++ gen/generator/makepad/src/widget/model/mod.rs
@@ -1,3 +1,7 @@
+use gen_converter::strategy::{id,class,inherits,style};
+
+use crate::gen;
+
 use self::live_design::LiveDesign;
 
 pub mod app_main;
@@ -12,4 +16,30 @@ pub mod traits;
 pub enum Model{
     AppMain(app_main::AppMain),
     Widget(widget::Widget),
+}
+
+impl Model {
+    pub fn new(mut model: gen_converter::model::Model)->Self{
+         // [这一部分是为了对Model进一步进行处理]-----------------------------------------------------
+        // 处理template部分
+        let _ = id(&mut model, gen::id());
+        let _ = class(&mut model, gen::class());
+        let _ = inherits(&mut model, gen::inherits());
+        // 处理style部分
+        let _ = style(&mut model, gen::style());
+        // [处理并生成script部分]------------------------------------------------------------------
+
+        if let Ok(sc) = script(
+            model,
+            gen::r#use(),
+            gen::prop(),
+            gen::event(),
+            gen::lifetime(),
+            gen::other(),
+        ) {
+            dbg!(sc);
+        }
+
+
+    }
 }
