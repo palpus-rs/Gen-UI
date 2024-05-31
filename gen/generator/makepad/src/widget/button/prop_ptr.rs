a217cf23c14f382612347b30ebc72f031d629823
syf20020816 <syf20020816@outlook.com>
2024-05-31 14:54:41 +0800

    button,label,icon,view,window ptr optimize macro

--- gen/generator/makepad/src/widget/button/prop_ptr.rs
+++ gen/generator/makepad/src/widget/button/prop_ptr.rs
@@ -2,27 +2,13 @@ use proc_macro2::TokenStream;
 use quote::ToTokens;
 use syn::{Fields, ItemStruct};
 
-use crate::{utils::struct_field, widget::utils::quote_makepad_widget_struct, ToToken};
+use crate::{
+    from_struct_to_ptr, ptr_to_token, utils::struct_field,
+    widget::utils::quote_makepad_widget_struct, ToToken,
+};
 
 pub struct ButtonPropPtr(pub ItemStruct);
 
-impl From<&ItemStruct> for ButtonPropPtr {
-    fn from(value: &ItemStruct) -> Self {
-        // 将GenUI的结构体转为Makepad的属性结构体
-        let mut new_item = quote_makepad_widget_struct(value);
-        // 设置#[deref]给当前的属性结构体
-        if let Fields::Named(fields) = &mut new_item.fields {
-            // add view
-            fields
-                .named
-                .push(struct_field(vec!["deref"], "button", "Button"));
-        }
-        ButtonPropPtr(new_item)
-    }
-}
+from_struct_to_ptr!(ButtonPropPtr, "button", "Button");
 
-impl ToToken for ButtonPropPtr {
-    fn to_token_stream(&self) -> TokenStream {
-        self.0.to_token_stream()
-    }
-}
+ptr_to_token!(ButtonPropPtr);
--- gen/generator/makepad/src/widget/view/prop_ptr.rs
+++ gen/generator/makepad/src/widget/view/prop_ptr.rs
@@ -2,27 +2,10 @@ use proc_macro2::TokenStream;
 use quote::ToTokens;
 use syn::{Fields, ItemStruct};
 
-use crate::{utils::struct_field, widget::utils::quote_makepad_widget_struct, ToToken};
+use crate::{from_struct_to_ptr, ptr_to_token, utils::struct_field, widget::utils::quote_makepad_widget_struct, ToToken};
 
 pub struct ViewPropPtr(pub ItemStruct);
 
-impl From<&ItemStruct> for ViewPropPtr {
-    fn from(value: &ItemStruct) -> Self {
-        // 将GenUI的结构体转为Makepad的属性结构体
-        let mut new_item = quote_makepad_widget_struct(value);
-        // 设置#[deref]给当前的属性结构体
-        if let Fields::Named(fields) = &mut new_item.fields {
-            // add view
-            fields
-                .named
-                .push(struct_field(vec!["deref"], "view", "View"));
-        }
-        ViewPropPtr(new_item)
-    }
-}
+from_struct_to_ptr!(ViewPropPtr,"view","View");
 
-impl ToToken for ViewPropPtr {
-    fn to_token_stream(&self) -> TokenStream {
-        self.0.to_token_stream()
-    }
-}
+ptr_to_token!(ViewPropPtr);
